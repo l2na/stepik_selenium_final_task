56061a3f5cc37a799abe1eb06bcdeced158a641c
Albina <albina_valieva7@mail.ru>
2022-06-20 16:16:29 +0300

    more_locators_update_base_page

--- pages/locators.py
+++ pages/locators.py
@@ -6,3 +6,10 @@ class MainPageLocators():
 class LoginPageLocators():
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
+
+class ProductPageLocators():
+    BASKET_BUTTON = (By.CSS_SELECTOR, ".btn-add-to-basket")
+    BOOK_NAME = (By.CSS_SELECTOR, '.breadcrumb .active')
+    MATCH_NAME = (By.CSS_SELECTOR, '#messages .alert:nth-child(1) .alertinner:nth-child(2) > strong')
+    BOOK_PRICE = (By.CSS_SELECTOR, '.product_main .price_color')
+    MATCH_PRICE = (By.CSS_SELECTOR, '.alert-info .alertinner p strong')
