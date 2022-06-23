1c86b7603e80955cd83045af4410c7c57692bbe3
Albina <albina_valieva7@mail.ru>
2022-06-23 16:04:20 +0300

    tests_for_success_message

--- test_product_page.py
+++ test_product_page.py
@@ -23,5 +23,24 @@ def test_guest_can_add_product_to_basket(browser, url):
     page.book_name_match()
     page.book_price_match()
 
+@pytest.mark.xfail
+def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_basket_page()
+    page.should_not_be_success_message()
 
+def test_guest_cant_see_success_message(browser):
+    link = "http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_not_be_success_message()
 
+@pytest.mark.xfail
+def test_message_disappeared_after_adding_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_basket_page()
+    page.should_disappear_success_message()
