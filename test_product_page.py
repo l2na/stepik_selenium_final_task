e32ac1deedde7a2679d8cbaccb86b9e00d0319e1
Albina <albina_valieva7@mail.ru>
2022-06-26 18:51:20 +0300

    added_tests_for_basket_page

--- test_product_page.py
+++ test_product_page.py
@@ -1,6 +1,7 @@
 import pytest
-
+from .pages.main_page import MainPage
 from .pages.product_page import ProductPage
+from .pages.basket_page import BasketPage
 from selenium.webdriver.common.by import By
 
 @pytest.mark.parametrize('url', ["?promo=offer0",
@@ -56,3 +57,21 @@ def test_message_disappeared_after_adding_product_to_basket(browser):
     page.open()
     page.go_to_basket_page()
     page.should_disappear_success_message()
+
+def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_basket_page()
+    basket_page = BasketPage(browser, browser.current_url)
+    basket_page.should_be_empty_basket()
+    basket_page.should_be_empty_basket_message()
+
+def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/ru/catalogue/the-shellcoders-handbook_209/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_basket_page()
+    basket_page = BasketPage(browser, browser.current_url)
+    basket_page.should_be_empty_basket()
+    basket_page.should_be_empty_basket_message()
