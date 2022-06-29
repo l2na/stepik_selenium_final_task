e1ac726ab6ad8b3f4cb2b375e60396d5ba9fbf4d
Albina <albina_valieva7@mail.ru>
2022-06-29 13:53:10 +0300

    added_setup_for_authorisation

--- test_product_page.py
+++ test_product_page.py
@@ -1,8 +1,45 @@
 import pytest
+import time
 from .pages.main_page import MainPage
 from .pages.product_page import ProductPage
 from .pages.basket_page import BasketPage
+from .pages.base_page import BasePage
+from .pages.login_page import LoginPage
 from selenium.webdriver.common.by import By
+
+@pytest.mark.new
+class TestUserAddToBasketFromProductPage():
+    @pytest.fixture(scope="function", autouse=True)
+    def setup(self, browser):
+        email = str(time.time()) + "@fakemail.org"
+        password = str(time.time())
+        link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
+        page = LoginPage(browser, link)
+        page.open()
+        page.register_new_user(email, password)
+        page = BasePage(browser, link)
+        page.should_be_authorized_user()
+
+    def test_user_cant_see_success_message(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/"
+        page = ProductPage(browser, link)
+        page.open()
+        page.should_not_be_success_message()
+
+    def test_user_can_add_product_to_basket(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+        page = ProductPage(browser, link)
+        page.open()
+        page.go_to_basket_page()
+        page.book_name_match()
+        page.book_price_match()
+
+
+def test_guest_cant_see_success_message(browser):
+    link = "http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_not_be_success_message()
 
 @pytest.mark.parametrize('url', ["?promo=offer0",
                                   "?promo=offer1",
@@ -30,12 +67,6 @@ def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
     page = ProductPage(browser, link)
     page.open()
     page.go_to_basket_page()
-    page.should_not_be_success_message()
-
-def test_guest_cant_see_success_message(browser):
-    link = "http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/"
-    page = ProductPage(browser, link)
-    page.open()
     page.should_not_be_success_message()
 
 def test_guest_should_see_login_link_on_product_page(browser):
